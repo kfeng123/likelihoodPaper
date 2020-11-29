0b3dbcd339a970a8b62c744e97aac6c49b7cf2b4
kfeng123 <446100240@qq.com>
2020-11-29 10:51:48 +0800

    NEW

--- main2020/code/python/symbolic_computation/tmp.py
+++ main2020/code/python/symbolic_computation/tmp.py
@@ -1,3 +1,4 @@
+import sympy as sp
 from sympy import *
 
 init_printing(use_unicode = True)
@@ -11,3 +12,30 @@ shouldBeZero = a2.subs([ (u,u0)])
 
 print("should be zero:")
 pprint(simplify(shouldBeZero))
+
+
+
+z, t, M = symbols("z t M")
+expr = 2**t * (1 + sp.exp(z * t * M)) / (1 + sp.exp(z * M ))**t
+expr = expr.subs([(M, 1)])
+a1 = diff(expr, z)
+a2 = diff(a1, z)
+a3 = diff(a2, z)
+a4 = diff(a3, z)
+
+
+print("first derivative:")
+pprint(simplify(a1))
+pprint(simplify(a1.subs([(z, 0), (M, 1)])))
+print("second derivative:")
+pprint(simplify(a2))
+pprint(simplify(a2.subs([(z, 0), (M, 1)])))
+print("third derivative:")
+pprint(simplify(a3))
+pprint(factor(a3))
+pprint(simplify(a3.subs([(z, 0), (M, 1)])))
+
+print("fourth derivative:")
+pprint(simplify(a4))
+pprint(factor(a4))
+pprint(simplify(a4.subs([(z, 0), (M, 1)])))
